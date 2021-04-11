064c7f5dfeadf51310eeb8c7d7701aec62dc0a8f
조혜수 <poer611@naver.com>
2021-04-12 01:21:33 +0900

    appbar 생성

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="white-nav">
+        내주변의 샐러드 가게
+      </div>
+    </div>
+  );
+}
+
+export default App;
